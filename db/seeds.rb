ec99f9a4f43cb853ef85acf583f8cbeb92a52194
Martin Dubuisson <martin.dbssn@gmail.com>
2024-05-21 17:15:20 +0200

    exercise finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,11 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Example:
-#
+10.times{
+@article = Article.new(title: Faker::Movie.title, content: Faker::Movie.quote)
+@article.save
+}
+
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
